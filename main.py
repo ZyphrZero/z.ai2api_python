4dd8966f74e4e22b5123cf4f30803915fe66087e
ZyphrZero <133507172+ZyphrZero@users.noreply.github.com>
2026-03-12 17:38:23 +0800

    Close reload...

--- main.py
+++ main.py
@@ -158,7 +158,7 @@ def run_server():
             interface="asgi",
             address="0.0.0.0",
             port=settings.LISTEN_PORT,
-            reload=True,  # 生产环境请关闭热重载
+            reload=False,  # 生产环境请关闭热重载
             process_name=service_name,  # 设置进程名称
             **RELOAD_CONFIG,  # 热重载配置
         ).serve()
